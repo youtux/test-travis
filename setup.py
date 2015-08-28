7723a4019b47c38177dc93624c9429853037d777
Alessio Bogon <youtux@gmail.com>
2015-08-28 19:21:48 +0200

    Bump version: 0.3.0-beta → 0.4.0-beta

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ setup(
     # Versions should comply with PEP440. For a discussion on
     # single-sourcing the version across setup.py and the project code,
     # see http://packaging.python.org/en/latest/tutorial.html#version
-    version='0.3.0-beta',
+    version='0.4.0-beta',
 
     description='Vanguard contains all the boilerplate you need to bootstrap a modern Python package.',
     long_description=parse_readme(),
